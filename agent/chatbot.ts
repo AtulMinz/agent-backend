1e712db31810ed7318ce18054d3ea322bb3f2ada
AtulMinz <atulminz.jsr@gmail.com>
2025-02-07 12:57:25 +0000

    Agent Setup

---
+++ agent/chatbot.ts
@@ -0,0 +1,19 @@
+import {
+  AgentKit,
+  CdpWalletActionProvider,
+  walletActionProvider,
+  cdpApiActionProvider,
+  erc20ActionProvider,
+  erc721ActionProvider,
+} from "@coinbase/agentkit";
+
+import { getLangChainTools } from "@coinbase/agentkit-langchain";
+import { HumanMessage } from "@langchain/core/messages";
+import { MemorySaver } from "@langchain/langgraph";
+import { createReactAgent } from "@langchain/langgraph/prebuilt";
+import { ChatOpenAI } from "@langchain/openai";
+import * as dotenv from "dotenv";
+import * as fs from "fs";
+import * as readline from "readline";
+
+dotenv.config();
